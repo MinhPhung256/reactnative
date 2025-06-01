dd738167c5e014d79963a1c12f27f61cc3a07a05
MinhPhung <minhphung.nt256@gmail.com>
2025-06-01 01:42:56 -0500

    commit lần 7

---
+++ components/Chat.js
@@ -0,0 +1,16 @@
+import {View, Text} from "react-native";
+
+const Chat =() =>{
+    return (
+        <View>
+            <Text>
+                Chat
+            </Text>
+        </View>
+
+    )
+
+}
+
+export default Chat;
+    
